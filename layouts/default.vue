880a8c9c6e8db5d3446dd0565c65bf77db799f24
Ganzhe <1959006169@qq.com>
2025-10-17 22:29:08 +0800

    defualtvue

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li><strong>Acme Corp</strong></li>
+      </ul>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Products</a></li>
+      </ul>
+    </nav>
+    <main>
+        <slot></slot>
+    </main>
+  </div>
+</template>
